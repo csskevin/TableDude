e6e7f61c84cabb77c1599c9d987815d678f592ea
Kevin Saiger <kevin@Kevins-MacBook-Pro.local>
2019-09-04 01:37:29 +0200

    Adding functionality for a simple parser

---
+++ src/Parser/SimpleParser.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace TableDude\Parser;
+
+class SimpleParser
+{
+    private $html;
+    private $doc;
+    private $xpath;
+    
+    public function __construct($html)
+    {
+        $this->html = $html;
+    }
+
+    public function parseHTMLTables()
+    {
+        $this->doc = new \DOMDocument();
+        $this->doc->loadHTML($this->html);
+        $this->xpath = new \DOMXpath($this->doc);
+        $tables = $this->xpath->query("//table");
+        $parsedTables = array();
+        foreach($tables as $table)
+        {
+            array_push($parsedTables, $this->parseTable($table));
+        }
+        return $parsedTables;
+    }
+
+    public function parseTable($table)
+    {
+        $parsedRows = array();
+        $rows = $this->xpath->query("*/tr|tr", $table);
+        foreach($rows as $row)
+        {
+            array_push($parsedRows, $this->parseRow($row));
+        }
+        return $parsedRows;
+    }
+
+    public function parseRow($row)
+    {
+        $parsedCells = array();
+        $cells = $this->xpath->query("td|th", $row);
+        foreach($cells as $cell)
+        {
+            array_push($parsedCells, $this->parseCell($cell));
+        }
+        return $parsedCells;
+    }
+
+    public function parseCell($cell)
+    {
+        $content = $cell->textContent;
+        $trimmedContent = trim($content);
+        return $trimmedContent;
+    }
+}
+
+?>
---
+++ src/Parser/SimpleParser.test.php
@@ -0,0 +1,132 @@
+<?php
+
+require("SimpleParser.php");
+
+use PHPUnit\Framework\TestCase;
+class FirstTest extends TestCase
+{
+    public function testMultipleTables()
+    {
+        $html = "<html>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Head1</th>
+                        <th>Head2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Value1</td>
+                        <td>Value2</td>
+                        <td>Value3</td>
+                    </tr>
+                    <tr>
+                        <td>Value4</td>
+                        <td>Value5</td>
+                        <td>Value6</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table>
+                <tr>
+                    <td>1Head1</td>
+                    <td>1Head2</td>
+                    <td>1Head3</td>
+                </tr>
+                <tr>
+                    <td>1Value1</td>
+                    <td>1Value2</td>
+                    <td>1Value3</td>
+                </tr>
+                <tr>
+                    <td>1Value4</td>
+                    <td>1Value5</td>
+                    <td>1Value6</td>
+                </tr>
+            </table>
+        </html>";
+        $expected = array(
+            array(
+                array(
+                    "Head1",
+                    "Head2"
+                ),
+                array(
+                    "Value1",
+                    "Value2",
+                    "Value3"
+                ),
+                array(
+                    "Value4",
+                    "Value5",
+                    "Value6"
+                )
+            ),
+            array(
+                array(
+                    "1Head1",
+                    "1Head2",
+                    "1Head3"
+                ),
+                array(
+                    "1Value1",
+                    "1Value2",
+                    "1Value3"
+                ),
+                array(
+                    "1Value4",
+                    "1Value5",
+                    "1Value6"
+                )
+            )
+        );
+        $tabledude = new TableDude\Parser\SimpleParser($html);
+        $tables = $tabledude->parseHTMLTables();
+        $this->assertEquals($tables, $expected);
+    }
+
+    public function testSingleTable()
+    {
+        $html = "<html>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Head1</td>
+                        <td>Head2</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> Value1</td>
+                        <td>Value2 </td>
+                    </tr>
+                    <tr>
+                        <td>Value4<i>Content</i></td>
+                        <td>Value5</td>
+                    </tr>
+                </tbody>
+            </table>
+        </html>";
+        $expected = array(
+            array(
+                array(
+                    "Head1",
+                    "Head2"
+                ),
+                array(
+                    "Value1",
+                    "Value2"
+                ),
+                array(
+                    "Value4Content",
+                    "Value5"
+                )
+            )
+        );
+        $tabledude = new TableDude\Parser\SimpleParser($html);
+        $tables = $tabledude->parseHTMLTables();
+        $this->assertEquals($tables, $expected);
+    }
+}
+?>
