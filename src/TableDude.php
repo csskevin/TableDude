0ada878de184d62f7c08decb1c19bc93ecd328a2
Kevin Saiger <kevin@Kevins-iMac.local>
2019-09-08 20:40:31 +0200

    Added autoloader

---
+++ src/TableDude.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . "/Parser/SimpleParser.php";
+require_once __DIR__ . "/Tools/ArrayTool.php";
+require_once __DIR__ . "/Converter/HorizontalTable.php";
+require_once __DIR__ . "/Converter/VerticalTable.php";
+require_once __DIR__ . "/Converter/MixedTable.php";
+require_once __DIR__ . "/Analysis/HeaderAnalyzation.php";
+
+?>
